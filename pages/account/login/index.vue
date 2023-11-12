98947452daab19cfafeb168424f3239d6c672b0e
anasssanba1 <sanbaanass73@gmail.com>
2023-11-12 12:28:26 +0100

    updated

--- pages/account/login/index.vue
+++ pages/account/login/index.vue
@@ -123,11 +123,6 @@ const LogOut = async () => {
 async function singInWithGoogle() {
 	const { data } = await supabase.auth.signInWithOAuth({
 		provider: "google",
-
-		options: {
-			redirectTo:
-				"https://e-commerce-nuxt-jmmyzykif-sanba-anass.vercel.app/account/login",
-		},
 	});
 }
 </script>
